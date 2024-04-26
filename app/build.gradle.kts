b9be9e432527c548d0df5c9f9d6ff298f319863d
Devarshi Patel <devarshi.patel@drcsystems.com>
2024-04-26 17:28:06 +0530

    google gson parsing and bug fixing

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -62,7 +62,7 @@ dependencies {
 
     // gson google
     // https://mvnrepository.com/artifact/com.google.code.gson/gson
-    implementation("com.google.code.gson:gson:2.10.1")
+    implementation(libs.gson)
 
 
 }
